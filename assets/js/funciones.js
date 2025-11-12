1bd6f859a9f85f3b66967ff604d016604f3b00da
Antonio Rodriguez Rodriguez <rodriguezdomingoantonio@gmail.com>
2025-11-12 01:05:03 -0300

    Desafio2 -- Condicionales --- avance al 35%

---
+++ assets/js/funciones.js
@@ -0,0 +1,30 @@
+
+function dsf1() {
+    var x = document.getElementById("dsf1");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function dsf2() {
+    var x = document.getElementById("dsf2");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function dsf3() {
+    var x = document.getElementById("dsf3");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
